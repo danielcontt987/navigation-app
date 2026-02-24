47633fb9c919f78127a394b715765f7d2564ba16
danielcontt987 <josedanielcontrerasperez510@gmail.com>
2026-02-24 12:08:18 -0600

    Colores personalizados con nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -3,11 +3,12 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <SafeAreaView>
+    <SafeAreaView >
       <View className="mt-5 mx-2.5">
-        <Text className="text-2xl font-work-black" >Hola mundo</Text>
-        <Text className="text-xl font-work-medium">Hola mundo</Text>
-        <Text className="text-xl font-work-light">Hola mundo</Text>
+        <Text className="text-4xl font-work-black text-primary" >Hola mundo</Text>
+        <Text className="text-3xl font-work-black text-secondary" >Hola mundo</Text>
+        <Text className="text-2xl font-work-medium text-secondary-100">Hola mundo</Text>
+        <Text className="text-xl font-work-light text-tertiary">Hola mundo</Text>
       </View>
     </SafeAreaView>
   );
